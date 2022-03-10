7a1571e90cd8ebfe6d0da529049af94c2b204500
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-09 22:56:45 -0300

    conteudo da pagina AboutMe

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -7,8 +7,11 @@ function AboutMe() {
         <>
             <InsertInfo InnerInfo={"Sobre Mim"} />
             <div className="aboutMeConteiner">
+                <article className="backgroundImgAboutMe">
+                    <img src="/images/mental-health.png" alt="backgroundImgAboutMe" />
+                </article>
                 <div className="mainAboutMeContent">
-
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque aliquid enim perspiciatis, in placeat sint corporis, iste distinctio, sequi officia error esse voluptas expedita illo nisi quod minus odit.</p>
                 </div>
             </div>
         </>

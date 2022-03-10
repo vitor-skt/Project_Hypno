81485495d03171f4a6f53d5a2f435c3b847720d8
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-10 13:48:51 -0300

    correções AboutMe

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -8,12 +8,11 @@ function AboutMe() {
             <InsertInfo InnerInfo={"Sobre Mim"} />
             <div className="aboutMeConteiner">
                 <div className="mainAboutMeContent">
-                    <p className="textAboutMe">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque aliquid enim perspiciatis, in placeat sint corporis, iste distinctio, sequi officia error esse voluptas expedita illo nisi quod minus odit.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nesciunt quaerat tempora minima ex, asperiores unde, iure iusto, consequuntur atque nisi optio quam dicta placeat quisquam harum nam laudantium debitis.</p>
+                    <div className="textBoxConteiner">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque aliquid enim perspiciatis, in placeat sint corporis, iste distinctio, sequi officia error esse voluptas expedita illo nisi quod minus odit.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nesciunt quaerat tempora minima ex, asperiores unde, iure iusto, consequuntur atque nisi optio quam dicta placeat quisquam harum nam laudantium debitis.</p>
+                    </div>
                 </div>
-                <article className="backgroundImgAboutMe">
-                    <img src="/images/mental-health.png" alt="backgroundImgAboutMe" />
-                </article>
             </div>
         </>
     )

cb02f0ee07dd7b187b6ddf2100828c07d98606e4
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-04-14 23:17:01 -0300

    adding video area

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -11,6 +11,11 @@ function AboutMe() {
                     <div className="textBoxConteiner">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque aliquid enim perspiciatis, in placeat sint corporis, iste distinctio, sequi officia error esse voluptas expedita illo nisi quod minus odit.
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nesciunt quaerat tempora minima ex, asperiores unde, iure iusto, consequuntur atque nisi optio quam dicta placeat quisquam harum nam laudantium debitis.</p>
+                        <div className="iframeContainer">
+                            <iframe width="540" height="315"
+                                src="https://www.youtube.com/embed/iik25wqIuFo" frameBorder="0" allowFullScreen title="VideoSample">
+                            </iframe>
+                        </div>
                     </div>
                 </div>
             </div>
